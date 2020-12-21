4edebe16eed85116ddec2bfda94536cf5134b18f
lassulus <lassulus@lassul.us>
2020-12-21 21:39:08 +0100

    shell.nix: use unstable

--- shell.nix
+++ shell.nix
@@ -1,4 +1,8 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { } }:
 pkgs.mkShell {
-  buildInputs = [ pkgs.tiled ];
+  buildInputs = [
+    (pkgs.writers.writeDashBin "edit-map" ''
+      ${pkgs.tiled}/bin/tiled ${toString ./map.json}
+    '')
+  ];
 }
