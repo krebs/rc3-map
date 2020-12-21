2cd1aa3e96bf05b23bf87e865208f4a549358e51
panda <p4nd4@posteo.de>
2020-12-21 23:23:53 +0100

    add missingtiles

---
+++ krebs.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="sterni_crate" tilewidth="32" tileheight="96" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <tile id="0">
+  <image width="32" height="32" source="../Documents/IllusUndArbeit/rc3/sterni_crate.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="96" source="../Documents/IllusUndArbeit/rc3/sterni_crates_1.png"/>
+ </tile>
+</tileset>
