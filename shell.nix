de2b4f363d70c4e9e0c74429f11ac949b5dd049b
lassulus <lassulus@lassul.us>
2020-12-24 15:55:35 +0100

    shell.nix: dont hardcode map path

--- shell.nix
+++ shell.nix
@@ -1,8 +1,6 @@
 { pkgs ? import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { } }:
 pkgs.mkShell {
   buildInputs = [
-    (pkgs.writers.writeDashBin "edit-map" ''
-      ${pkgs.tiled}/bin/tiled ${toString ./map.json}
-    '')
+    pkgs.tiled
   ];
 }
